4fbc22c037fddcd079cfa9260e99c68331be3353
RenoirTan <renoirtan2005@gmail.com>
2023-01-21 17:12:12 +0800

    main: use constants instead of strings

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ import express from "express";
 import knex from "knex";
 
 
+const DBPATH = "db/todos.db";
+const TODOS = "Todos";
+
+
 const app = express();
 app.set("view engine", "pug");
 app.use(bodyParser.json());
@@ -12,15 +16,15 @@ app.use(bodyParser.urlencoded({extended: true}));
 const client = knex({
   client: "sqlite3",
   connection: {
-    filename: "db/todos.db"
+    filename: DBPATH
   },
   useNullAsDefault: true
 });
 
 
-let todosExist = await client.schema.hasTable("Todos");
+let todosExist = await client.schema.hasTable(TODOS);
 if (!todosExist) {
-  await client.schema.createTable("Todos", table => {
+  await client.schema.createTable(TODOS, table => {
     table.increments("id", {primaryKey: true}).unique().notNullable();
     table.string("title", 64); // Set max length to 64
     table.text("body")
@@ -31,7 +35,7 @@ if (!todosExist) {
 app.get("/", async (req, res) => {
   const result = await client
     .select("id", "title")
-    .from("Todos");
+    .from(TODOS);
   res.render("index", {todoItems: result});
 });
 
@@ -39,7 +43,7 @@ app.get("/item/:id", async (req, res) => {
   const id = req.params.id;
   const result = await client
     .select("*")
-    .from("Todos")
+    .from(TODOS)
     .where("id", id);
   if (result.length == 0) {
     res.status(404).render("404");
@@ -50,7 +54,7 @@ app.get("/item/:id", async (req, res) => {
 
 app.post("/item/delete/:id", async (req, res) => {
   const id = req.params.id;
-  const result = await client("Todos").delete().where("id", id);
+  const result = await client(TODOS).delete().where("id", id);
   if (result == 1) {
     res.redirect("/");
   } else {
@@ -65,7 +69,7 @@ app.get("/new", async (req, res) => {
 app.post("/new", async (req, res) => {
   const title = req.body.title;
   const body = req.body.body;
-  const result = await client("Todos").insert({title: title, body: body});
+  const result = await client(TODOS).insert({title: title, body: body});
   if (result.length == 0) {
     res.status(500).render("500", {errMsg: "Couldn't add item!"});
   } else {
